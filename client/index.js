0e055b017db547189fdf7149e5130babbc2862f0
Abhishek Jain <abhi2000@gmail.com>
2017-12-15 22:51:16 +0000

    add html webpack plugin to create html file

--- client/index.js
+++ client/index.js
@@ -1,1 +1,5 @@
-const a = x => x * x;
+const square = x => x * x;
+
+const value = square(10);
+
+document.write(`${value} is the square of 10`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
-// @flow
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 module.exports = (env) => {
-    return {
-        entry: './client/index.js',
-        output: {
-            filename: 'dist/bundle.js'
-        },
-        module: {
-            rules: [
-                { test: /\.js$/, use: 'babel-loader' }
-            ]
-        }
-    }
+  return {
+    entry: './client/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {test: /\.js$/, use: 'babel-loader'}
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({template: './public/index.html'})
+    ]
+  }
 }
