79985d3bbea85221fee647dad872db1779ac0c8d
Abhishek Jain <abhi2000@gmail.com>
2018-01-21 22:21:16 +0000

    add chunking

--- client/index.js
+++ client/index.js
@@ -12,9 +12,10 @@ const renderApp = () => {
 };
 
 renderApp();
-
-if (module.hot) {
-  module.hot.accept('./app.js', () => {
-    renderApp();
-  })
+if (process.env.NODE_ENV !== 'production') {
+  if (module.hot) {
+    module.hot.accept('./app.js', () => {
+      renderApp();
+    })
+  }
 }
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,10 +4,12 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const {scssProdRule, babelLoader} = require('./config/webpack/loaders');
 const path = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = (env) => {
   return {
     entry: {
+      'vendor': ['react', 'react-dom', 'react-router', 'react-router-dom'],
       'main': './client/index.js',
       'home': './client/home/home.js',
       'about': './client/about/about.js',
@@ -31,13 +33,15 @@ module.exports = (env) => {
       new HtmlWebpackPlugin({template: './public/index.html'}),
       new webpack.NamedModulesPlugin(),
       new webpack.optimize.CommonsChunkPlugin({
-        name: 'common' // Specify the common bundle's name.
+        name: 'vendor', // Specify the common bundle's name.
+        minChunks: Infinity
       }),
       new ExtractTextPlugin('css/[name]-[contenthash:8].css'),
       new BundleAnalyzerPlugin({
         analyzerMode: 'static',
         openAnalyzer: false
-      })
+      }),
+      new UglifyJSPlugin()
     ]
   }
 }
